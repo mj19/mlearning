c23da68588f3363630c830cdb8894a615a992188
mj19 <30759135@qq.com>
2018-07-07 11:47:48 +0800

    perceptron2.py
    perceptron implemention in 统计学习方法

---
+++ perceptron2.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+p_x = np.array([[3, 3], [4, 3], [1, 1]])
+y = np.array([1, 1, -1])
+
+plt.figure()
+
+for i in range(len(p_x)):
+    if y[i] == 1:
+        plt.plot(p_x[i][0], p_x[i][1], 'ro')
+    else:
+        plt.plot(p_x[i][0], p_x[i][1], 'bo')
+
+w = np.array([1, 0])
+b = 0
+delta = 1
+
+for i in range(100):
+    choice = -1
+    for j in range(len(p_x)):
+        if y[j] != np.sign(np.dot(w, p_x[0]) + b):
+            choice = j
+            break
+    if choice == -1:
+        break
+    w = w + delta * y[choice] * p_x[choice]
+    b = b + delta * y[choice]
+
+line_x = [0, 10]
+line_y = [0, 0]
+
+for i in range(len(line_x)):
+    line_y[i] = (-w[0] * line_x[i] - b) / w[1]
+
+plt.plot(line_x, line_y)
+plt.show()
+
+# matplotlib.pyplot.figure - create a figure
+# matplotlib.pyplot.plot - draw dot in figure
+# numpy.array - create a new array
+# numpy.dot 矩阵乘法 doc product of 2 array
+# numpy.sign
+
